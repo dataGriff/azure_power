ec291e432dc4469b30ac3e83dc91f1217764bbb3
RICHARD GRIFFITHS <griff182uk@yahoo.co.uk>
2022-02-23 19:02:23 +0000

    added function to power down event hubs

---
+++ EventHubPowerDown/run.ps1
@@ -0,0 +1,41 @@
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+## remember to give msi on the function
+## remember to give msi permissions on the sub
+
+Write-Host ("Starting function to power down event hub namespaces...")
+
+$scalemin = 1
+$namespaces = Get-AzEventHubNamespace
+
+foreach ($namespace in $namespaces)
+{
+$name = $namespace.Name
+$resourceGroup = $namespace.ResourceGroup
+$location = $namespace.Location
+$scaletag = $namespace.Tags['ScaleMin']
+
+Write-Host ("Name: $name")
+Write-Host ("Resource Group: $resourceGroup")
+Write-Host ("Location: $location")
+Write-Host ("ScaleMin: $scaletag")
+
+    if(!$scaletag)
+    {
+        Write-Host("Adding ScaleMin tag of $scalemin to namespace $name...")
+        Update-AzTag -ResourceId $namespace.Id `
+        -Tag @{ScaleMin=$scalemin;}
+        Write-Host("Added ScaleMin tag of $scalemin to namespace $name.")
+        $scaletag = $scalemin
+    }
+
+    Write-Host("Scaling namespace $name to $scalemin throughput units...")
+    Set-AzEventHubNamespace -Name $name `
+    -ResourceGroupName $resourceGroup `
+    -Location $location `
+    -SkuCapacity $scaletag
+    Write-Host("Scaled namespace $name to $scalemin throughput units.")
+}
+
+Write-Host ("Ended function to power down event hub namespaces.")
---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+# Description
+
+Aiming to create suite of functionality to power down Azure assets. 
+
+## Requirements
+
+* Must created managed identity
+* Must give managed identity permissions on resources its going to interact with
